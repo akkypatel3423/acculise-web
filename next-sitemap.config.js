cfda700518fbf4d87f09835254e05dfb877fdf12
akash-kanani <akash-kanani>
2026-02-14 14:28:22 +0530

    Added sitemap robot and llm txt file

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.acculise.com",
+  generateRobotsTxt: true,
+  generateIndexSitemap: true,
+  sitemapSize: 7000,
+
+  changefreq: "daily",
+  priority: 0.7,
+};
